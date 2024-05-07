1166c4934e60b5b87bdf80faba52f899da3b7468
YomnaAwny <108230947+YomnaAwny@users.noreply.github.com>
2024-05-07 23:25:24 +0300

    Add files via upload

---
+++ RCC/RCC_Interface.h
@@ -0,0 +1,18 @@
+/*
+ * RCC_Interface.h
+ *
+ *  Created on: May 7, 2024
+ *      Author: Yomna Awmy Salh
+ */
+
+#ifndef RCC_RCC_INTERFACE_H_
+#define RCC_RCC_INTERFACE_H_
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "RCC_Regis.h"
+#include "RCC_Config.h"
+
+
+#endif /* RCC_RCC_INTERFACE_H_ */
